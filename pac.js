22658d24b4b1000c4a63629845aecf69b9e0308f
Xavier <xavierror@aliyun.com>
2022-08-30 10:15:08 +0800

    init commit

---
+++ pac.js
@@ -0,0 +1,34 @@
+function FindProxyForURL(url, host) {
+  const DIRECT = 'DIRECT';
+  const proxy_yes = 'SOCKS5 127.0.0.1:7890';
+
+  if (isPlainHostName(host)) return DIRECT;
+
+  const hosts = [
+    '*bgm.tv',
+    '*asmr.one',
+    '*v2ex.*',
+    '*npmjs.com',
+    '*w3.org',
+    '*wikipedia.org',
+    '*github*',
+    '*google*',
+    '*googlevideo.com',
+    '*youtube*',
+    '*postman*',
+    '*ytimg*',
+    '*dmhy*',
+    '*ruanyifeng.com',
+    'duckduckgo.com',
+    'steamcommunity.com',
+    '*.steampowered.com',
+    '*xargin.com',
+    '*t.me',
+  ];
+
+  for (const _host of hosts) {
+    if (shExpMatch(host, _host)) return proxy_yes;
+  }
+
+  return DIRECT;
+}
