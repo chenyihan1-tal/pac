b5e2a460449e7943907bcaa2be047d527d7acb6c
Xavier <xavierror@aliyun.com>
2024-04-24 09:44:16 +0800

    Update dk.js

--- dk.js
+++ dk.js
@@ -1,7 +1,7 @@
 device.wakeUp();
 auto.waitFor();
 
-var storage = storages.create("CLOCK_LOG");
+var storage = storages.create('CLOCK_LOG');
 
 var doback = () => {
   for (var i = 0; i < 5; i++) {
@@ -12,19 +12,19 @@ var doback = () => {
   sleep(500);
 };
 
-var doclock = (canclock) => {
+var doclock = canclock => {
   // 点击打卡
   if (canclock) {
     // 打开APP
-    launchApp("知音楼");
+    launchApp('知音楼');
     sleep(5000);
     doback();
     sleep(1000);
-    launchApp("知音楼");
+    launchApp('知音楼');
     sleep(5000);
 
     // 点击工作台
-    id("tv_tab_title").text("工作台").findOne().parent().click();
+    id('tv_tab_title').text('工作台').findOne().parent().click();
     sleep(5000);
 
     // 点击考勤打卡
@@ -38,6 +38,7 @@ var doclock = (canclock) => {
   }
 
   // 截图
+  sleep(1000);
   takeScreenshot();
   sleep(1000);
 
@@ -46,18 +47,22 @@ var doclock = (canclock) => {
   sleep(2000);
 
   // 点击分享到QQ
-  text("QQ").findOne().parent().click();
+  text('QQ').findOne().parent().click();
   sleep(1000);
 
-  text("XAVIER").findOne().parent().click();
+  text('XAVIER').findOne().parent().click();
   sleep(1000);
 
-  text("发送").findOne().click();
+  text('发送').findOne().click();
   sleep(1000);
 
   // 返回首页
   doback();
 };
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
 
 for (;;) {
   auto.waitFor();
@@ -67,47 +72,48 @@ for (;;) {
   device.setBrightness(0); // 设置屏幕亮度为最低;
 
   // 给我返回首页的时间
+  home();
   sleep(3000);
 
   var now = new Date();
-  var date = now.getDate();
   var hour = now.getHours();
   var minute = now.getMinutes();
-  var nowStr = new java.text.SimpleDateFormat("HH:mm").format(now);
+  var date = new java.text.SimpleDateFormat('yyyy/MM/dd').format(now);
 
-  var canClockin =
-    hour === 9 &&
-    minute >= 15 &&
-    minute <= 30 &&
-    storage.get(`${date}_clockin`) !== 1;
+  // 两个小时的毫秒数
+  var twoHours = 1000 * 60 * 60 * 2;
+  // 早上打卡时间
+  var clockinTime = new Date(`${date} 09:15:00`).getTime() + random(1000 * 60 * 3, 1000 * 60 * 10);
+  // 晚上打卡时间
+  var clockoutTime = new Date(`${date} 20:00:00`).getTime() + random(1000 * 60 * 5, 1000 * 60 * 10);
 
-  var canClockout =
-    hour === 20 &&
-    minute >= 5 &&
-    minute <= 30 &&
-    storage.get(`${date}_clockout`) !== 1;
+  var r = 0;
 
-  // 随机睡眠7-13分钟
-  var r = Math.ceil(Math.random() * 6 + 7);
-  if (hour !== 9 && hour !== 20) {
-    r = 60;
-    if (hour === 8) {
-      r = 20;
-    }
+  // 如果当前时间小于早上打卡时间
+  if (now.getTime() < clockinTime) {
+    r = Math.min(clockinTime - now.getTime(), twoHours);
   }
-
-  toastLog(`${nowStr} ${r} ${canClockin} ${canClockout}`);
-  sleep(500);
-
-  if (canClockin) {
+  // 如果早上还没有打过卡
+  else if (hour === 9 && storage.get(`${date}_clockin`) !== 1) {
     doclock(true);
     storage.put(`${date}_clockin`, 1);
-  } else if (canClockout) {
+  }
+  // 如果当前时间小于晚上打卡时间
+  else if (now.getTime() < clockoutTime) {
+    r = Math.min(clockoutTime - now.getTime(), twoHours);
+  }
+  // 如果晚上还没有打过卡
+  else if (hour === 20 && storage.get(`${date}_clockout`) !== 1) {
     doclock(true);
     storage.put(`${date}_clockout`, 1);
   } else {
-    doclock(false);
+    r = twoHours * 2;
   }
 
-  sleep(r * 60 * 1000);
+  if (r > 0) {
+    toastLog('等待时间：' + Math.ceil(r / 1000 / 60) + '分钟');
+    doclock(false);
+    sleep(r);
+  }
+  continue;
 }
